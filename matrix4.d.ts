477a03d1cb9003314a3363c4e29ef4d59e232e11
3Dlyra <3dlyra.com@gmail.com>
2023-02-11 16:42:24 +0500

    Implement `fromQuaternion` method

--- matrix4.d.ts
+++ matrix4.d.ts
@@ -2,3 +2,4 @@ export function create(): Float32Array;
 export function translate( m4: Float32Array, v3: Float32Array ): Float32Array;
 export function rotateAxis( m4: Float32Array, v3: Float32Array, radian: number ): Float32Array;
 export function scale( m4: Float32Array, v3: Float32Array ): Float32Array;
+export function fromQuaternion( q: Float32Array ): Float32Array;
