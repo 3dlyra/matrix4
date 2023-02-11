11d8822233357ea3226d83d12d644024e162863a
3Dlyra <3dlyra.com@gmail.com>
2023-02-11 16:55:29 +0500

    Implement `applyToVector3` method

--- matrix4.d.ts
+++ matrix4.d.ts
@@ -3,3 +3,4 @@ export function translate( m4: Float32Array, v3: Float32Array ): Float32Array;
 export function rotateAxis( m4: Float32Array, v3: Float32Array, radian: number ): Float32Array;
 export function scale( m4: Float32Array, v3: Float32Array ): Float32Array;
 export function fromQuaternion( q: Float32Array ): Float32Array;
+export function applyToVector3( m4: Float32Array, v3: Float32Array ): Float32Array;
